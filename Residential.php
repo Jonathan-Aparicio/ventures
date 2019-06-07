95dc35b195aed7ed798a175c486aa2f8f69b4de4
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-07 14:50:00 -0600

    fixed errors in residential.php

--- Residential.php
+++ Residential.php
@@ -3,7 +3,7 @@
   $dao = new Dao();
   // $info = array();
  $type = 0;
- $info =  $dao->getMainHousePhotos($id, $type);
+ $info =  $dao->getMainHousePhotos($type);
  $size = count($info);
  echo "$size";
  ?>
@@ -15,30 +15,19 @@
 
 <div class="container">
   <?php for ($x = 0; $x <= $size; $x++) {
-            echo " <div class="row">
-                <div class="col-sm-4">
-                  <img class="img-responsive" src="$info[$x]['MainPhoto']">
-                  bage house
+            echo " <div class=\"row\">
+                <div class=\"col-sm-4\">
+                  <img class=\"img-responsive\" src=\"" . $info[$x]['MainPhoto'] . "\">
                 </div>
-                <div class="col-sm-4">
-                  <img class="img-responsive" src="$info[$x+1]['MainPhoto']">
+                <div class=\"col-sm-4\">
+                  <img class=\"img-responsive\" src=\"" . $info[$x+1]['MainPhoto'] . "\">
                 </div>
-                <div class="col-sm-4">
-                  <img class="img-responsive" src="$info[$x+2]['MainPhoto']">
+                <div class=\"col-sm-4\">
+                  <img class=\"img-responsive\" src=\"" . $info[$x+2]['MainPhoto'] . "\">
                 </div>";
 
           $x= $x + 2;
         }
   ?>
-  <!-- // <div class="row">
-  //   <div class="col-sm-4">
-  //     <img class="img-responsive" src="bage-house.jpg">
-  //     bage house
-  //   </div>
-  //   <div class="col-sm-4">
-  //     <img class="img-responsive" src="white-house.jpg">
-  //   </div>
-  //   <div class="col-sm-4">
-  //     <img class="img-responsive" src="blue-house.jpg">
-  //   </div> -->
+
   </body>
