81c32cc2256a8e390aaeca11cd005433e2d9cb7a
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-11 13:46:13 -0600

    a

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
 
 <body>
   <div class="container">
-    <form action="upload-manager.php" method="post" enctype="multipart/form-data" action="uploadHandler.php">
+    <form action="uploadHandler.php" method="post" enctype="multipart/form-data" >
        <h2>Upload File</h2>
        <label for="fileSelect">Filename:</label>
        <input type="file" name="photo" id="fileSelect">
