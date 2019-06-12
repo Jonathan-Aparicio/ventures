dd9566c3bd9d229c14638c03ef3a6d0c6b54bc60
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-12 16:07:31 -0600

    make text look better

--- Residential.php
+++ Residential.php
@@ -17,7 +17,7 @@
             echo " <div class=\"row\">
                 <div class=\"col-sm-4\">
                   <img class=\"img-responsive\" src=\"" . $info[$x]['MainPhoto'] . "\">
-                  " . $info[$x]['StreetAddress'] . " " . $info[$x]['City'] . " " . $info[$x]['State'] . "
+                  <h3><p class=\"text-center\">" . $info[$x]['StreetAddress'] . " " . $info[$x]['City'] . " " . $info[$x]['State'] . "</p></h3>
                 </div>
                 <div class=\"col-sm-4\">
                   <img class=\"img-responsive\" src=\"" . $info[$x+1]['MainPhoto'] . "\">
