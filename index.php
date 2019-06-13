3b999ca7b66af84b104656a3be143cf318806cc6
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-13 13:31:41 -0600

    new photos

--- index.php
+++ index.php
@@ -4,17 +4,22 @@
 </div>
 <body>
   <div class="container">
+    <div class="jumbotron">
+  <!-- <h1>Bootstrap Tutorial</h1> -->
+  <p class="text-center">Welcom to CADAVentures rentals curently this website is a school project. All pictures being used are stock photos
+    </p>
+</div>
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- <div id="myCarousel" class="carousel slide" data-ride="carousel"> -->
       <!-- Indicators -->
-      <ol class="carousel-indicators">
+      <!-- <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
+      </ol> -->
 
       <!-- Wrapper for slides -->
-      <div class="carousel-inner">
+      <!-- <div class="carousel-inner">
         <div class="item active">
           <img src="bage-house.jpg" alt="Los Angeles" style="width:100%; height:50%">
         </div>
@@ -26,10 +31,10 @@
         <div class="item">
           <img src="blue-house.jpg" alt="New york" style="width:100%; height:50%">
         </div>
-      </div>
+      </div> -->
 
       <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
         <span class="sr-only">Previous</span>
       </a>
@@ -37,7 +42,7 @@
         <span class="glyphicon glyphicon-chevron-right"></span>
         <span class="sr-only">Next</span>
       </a>
-    </div>
+    </div> -->
   </div>
 
 
