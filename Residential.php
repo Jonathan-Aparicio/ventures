7a5af70b46e662318bb83b7a288834e7f82d76a0
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-18 16:39:55 -0600

    a

--- Residential.php
+++ Residential.php
@@ -16,15 +16,15 @@
             echo " <div class=\"row\">
                 <div class=\"col-sm-4\">
                   <a href=\"generalHouse.php?id=".$info[$x]['ID']."\" >
-                  <img class=\"img-responsive\" src=\"" . $info[$x]['MainPhoto'] . "\">
+                  <img class=\"img-fluid\" src=\"" . $info[$x]['MainPhoto'] . "\">
                   <h3><p class=\"text-center\">" . $info[$x]['StreetAddress'] . " " . $info[$x]['City'] . " " . $info[$x]['State'] . "</p></h3></a>
                 </div>
                 <div class=\"col-sm-4\">
-                  <img class=\"img-responsive\" src=\"" . $info[$x+1]['MainPhoto'] . "\">
+                  <img class=\"img-fluid\" src=\"" . $info[$x+1]['MainPhoto'] . "\">
                   <h3><p class=\"text-center\">" . $info[$x+1]['StreetAddress'] . " " . $info[$x+1]['City'] . " " . $info[$x+1]['State'] . "</p></h3>
                 </div>
                 <div class=\"col-sm-4\">
-                  <img class=\"img-responsive\" src=\"" . $info[$x+2]['MainPhoto'] . "\">
+                  <img class=\"img-fluid\" src=\"" . $info[$x+2]['MainPhoto'] . "\">
                   <h3><p class=\"text-center\">" . $info[$x+2]['StreetAddress'] . " " . $info[$x+2]['City'] . " " . $info[$x+2]['State'] . "</p></h3>
                 </div>";
 
--- generalHouse.php
+++ generalHouse.php
@@ -16,7 +16,7 @@
 <div class="container">
   <div class="row justify-content-center">
     <!-- <div class="col-3"></div> -->
-    <div class="col-sm-6"><img class="img-responsive" src="<?php echo $info[0]['MainPhoto'];?>"></div>
+    <div class="col-sm-6"><img class="img-fluid" src="<?php echo $info[0]['MainPhoto'];?>"></div>
     <!-- <div class="col-3"></div> -->
   </div>
 
@@ -29,13 +29,13 @@
   <?php for ($x = 0; $x <= $size; $x++) {
             echo " <div class=\"row\">
                 <div class=\"col-sm-4\">
-                  <img class=\"img-responsive\" src=\"" . $pics[$x]['image'] . "\">
+                  <img class=\"img-fluid\" src=\"" . $pics[$x]['image'] . "\">
                 </div>
                 <div class=\"col-sm-4\">
-                  <img class=\"img-responsive\" src=\"" . $pics[$x+1]['image'] . "\">
+                  <img class=\"img-fluid\" src=\"" . $pics[$x+1]['image'] . "\">
                 </div>
                 <div class=\"col-sm-4\">
-                  <img class=\"img-responsive\" src=\"" . $pics[$x+2]['image'] . "\">
+                  <img class=\"img-fluid\" src=\"" . $pics[$x+2]['image'] . "\">
                 </div>";
 
           $x= $x + 2;
