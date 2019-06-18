e3733c15c207d73e6556a47994abef8cfdd34aed
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-18 13:53:38 -0600

    back to 3.4

--- nav-bar.php
+++ nav-bar.php
@@ -3,24 +3,22 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="index.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">CADAVentures</a>
     </div>
-    <ul class="navbar-nav">
-      <li class="nav-item"><a href="index.php">Home</a></li>
-      <li class="nav-item"><a href="Residential.php">Residential</a></li>
-      <li class="nav-item"><a href="#">Commercial</a></li>
-      <li class="nav-item"><a href="#">Industrial</a></li>
-      <li class="nav-item"><a href="upload.php">upload</a></li>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="Residential.php">Residential</a></li>
+      <li><a href="#">Commercial</a></li>
+      <li><a href="#">Industrial</a></li>
+      <li><a href="upload.php">upload</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
