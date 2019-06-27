c7f7c46a7555cc07e08b5cb1b1655ff7015b5b32
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-06-27 14:41:30 -0600

    try out readonly text for maintence

--- Maintenance.php
+++ Maintenance.php
@@ -4,12 +4,13 @@
   </div>
 
   <div class="container">
-    <div class="row align-items-center">
+    <div class="row align-items-center justify-content-center">
       <div class="col-sm-6">
         <form>
           <div class="form-group">
             <label for="emal">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <input type="emal" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo @$_SESSION['email'];?>">
+            <!-- <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"> -->
           </div>
           <div class="form-group">
             <label for="general description">Example textarea</label>
