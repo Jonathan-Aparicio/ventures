215e03770a8b31247b0c637b99fd4e44289086cd
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-02 11:57:13 -0600

    first test for email

--- Maintenance.php
+++ Maintenance.php
@@ -10,17 +10,17 @@
   <div class="container">
     <div class="row align-items-center justify-content-center" style="height:100%">
       <div class="col-sm-8">
-        <form>
+        <form method="POST" action="Maintenance-handler.php">
           <div class="form-group row">
-            <label for="emal" class="col-sm-2 col-form-label">Email address</label>
+            <label for="email" class="col-sm-2 col-form-label">Email address</label>
             <div class="col-sm-10">
-            <input type="emal" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo @$_SESSION['email'];?>">
+            <input type="email" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo @$_SESSION['email'];?>">
             </div>
             <!-- <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"> -->
           </div>
           <div class="form-group">
             <label for="general description">Example textarea</label>
-            <textarea class="form-control" id="generalDescription" name="generalDescription" rows="3"></textarea>
+            <textarea class="form-control" id="generalDescription" name="generalDescription" rows="5"></textarea>
           </div>
         </form>
       </div>
