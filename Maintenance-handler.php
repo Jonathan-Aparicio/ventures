5f2799bb8cb7a2980863fccf954b5a467c9abc56
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-03 16:06:23 -0600

    a

--- Maintenance-handler.php
+++ Maintenance-handler.php
@@ -9,10 +9,10 @@ require 'fpdf.php';
 
 //create pdf
 $pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,$info);
-$pdf->Output('test.pdf,I');
+// $pdf->AddPage();
+// $pdf->SetFont('Arial','B',16);
+// $pdf->Cell(40,10,$info);
+// $pdf->Output('test.pdf,I');
 
 
 //Mail
