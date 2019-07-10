4b00ff118ef97ad528281a687606397f4a37a53f
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-10 14:03:36 -0600

    a

--- Maintenance-handler.php
+++ Maintenance-handler.php
@@ -8,6 +8,21 @@ use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
 require 'fpdf.php';
 
+// function title(){
+//   $this->SetFont('Arial','BI',20);
+//   $this->Cell(0,10,"CADAVenturs",1,1,'C');
+//   $this->LN();
+//   $this->LN();
+// }
+// fuction body($i, $s){
+//   $this->SetFont('Arial',14);
+//   $this->Cell(0,0,$s);
+//   $this->LN();
+//   $this->Cell(0,0,$i);
+//   $this->LN();
+//   $this->Cell(0,0,date("y-m-d"));
+// }
+
 $path = str_replace(' ', '', $sub). '.pdf';
 // echo $path;
 // $path = "Maintenance".date("y-m-d") .".pdf";
@@ -16,14 +31,16 @@ $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','BI',20);
 $pdf->Cell(0,10,"CADAVenturs",1,1,'C');
-$pdf->LN()
-$pdf->LN()
+$pdf->LN();
+$pdf->LN();
 $pdf->SetFont('Arial',14);
-// $pdf->Cell(0,0,$sub);
-// $pdf->LN()
-// $pdf->Cell(0,0,$info);
-// $pdf->LN()
-// $pdf->Cell(0,0,date("y-m-d"));
+$pdf->Cell(0,0,$sub);
+$pdf->LN();
+$pdf->Cell(0,0,$info);
+$pdf->LN();
+$pdf->Cell(0,0,date("y-m-d"));
+// $pdf->title();
+// $pdf->body($info,$sub);
 $pdf->Output($path,'F');
 
 // if(file_exists($path)) echo 'pdf created ' . $path;
