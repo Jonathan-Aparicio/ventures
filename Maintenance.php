5f61072a3fd046a5f8ea5ddd3c54c22931252275
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-12 11:49:02 -0600

    got address to work

--- Maintenance.php
+++ Maintenance.php
@@ -13,27 +13,14 @@
   </div>
 
   <div class="container">
-    <?php if($user['Renting'] != NULL):
-
-      $address = $dao->getInfo((int)$user['Renting']);
-      if(empty($address) ){
-      echo "<div><p>empty</p></div>";
-    }else{
-      echo "<div><p>not empty</p></div>";
-    }?>
+    <?php if($user['Renting'] != NULL): $address = $dao->getInfo((int)$user['Renting']);?>
     <div class="row align-items-center justify-content-center" style="height:100%">
       <div class="col-sm-8">
         <form method="POST" action="Maintenance-handler.php">
           <div class="form-group row">
             <label for="email" class="col-sm-2 col-form-label">Email address</label>
             <div class="col-sm-10">
-            <input type="email" name="email" readonly class="form-control-plaintext" id="staticEmail" value="<?php //echo @$_SESSION['email'];
-            if(empty($address) ){
-            echo "empty";
-          }else{
-            echo $user['Renting'];
-          }
-            ?>">
+            <input type="email" name="email" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo @$_SESSION['email'];?>">
             </div>
           </div>
           <div class="form-group row">
