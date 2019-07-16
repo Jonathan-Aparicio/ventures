7220cdfeb20233ecdf75b2c592f329ac209ba8c6
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-16 13:42:31 -0600

    more to footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <div class="row justify-content-center">
+    <div class="col-sm-6">
+      <h4><p class="text-center"> CADAVenturs</p></h4>
+      <p class="text-center"> Thank you for checking out our rentals. If you have questions please feel free to contact us.</p>
+    </div>
+    <div class="col-sm-6">
+      <p class="text-center">Email:</p>
+      <p class="text-center">cadaventuresmaintenance@gmail.com</p>
+    </div>
+  </div>
+</div>
