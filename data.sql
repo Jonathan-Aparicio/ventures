bb6f14c69d78ef6e4cbe7f8b131c65a4c2f69723
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-17 11:49:02 -0600

    try footer class

--- data.sql
+++ data.sql
@@ -20,17 +20,29 @@ insert into Houses
     
 	insert into Houses
 	(ID,  StreetAddress, City, State ,MainPhoto, HouseTypeID ,PhotoID, Description)
-    values(3, '5643 main st', 'boise', 'id' , 'images\\res\\tiny\\tiny-house.jpg', 'com', 3, 'store font ');
+    values(3, '5643 main st', 'boise', 'id' , 'images\\com\\green\\green-storefront.jpg', 'com', 3, 'store font ');
     
     insert into Houses
 	(ID,  StreetAddress, City, State ,MainPhoto, HouseTypeID ,PhotoID, Description)
-    values(4, '3479 rich st', 'boise', 'id' , 'images\\res\\mondern\\mondern-house.jpg', 'res', 4, '$$$$$ – ## Bed / ## Bath apartment|house|condo|duplex in neighborhood with great feature.<br>
+    values(4, '3479 rich st', 'boise', 'id' , 'images\\res\\mondern\\mondern-house.jpg', 'res', 4, '$$$$$  ## Bed / ## Bath apartment|house|condo|duplex in neighborhood with great feature.<br>
     Great views of the area: Charming, naturally well-lit # bedroom # bathroom apartment|house|condo|duplex. Close to schools, shops and nightlife, and walking distance 
     to the grocery stores. Balcony/porch for entertaining, hardwood/carpet flooring throughout.<br> apartment|house|condo|duplex inclueds full kitchen and has heating and air.');
+    
+	insert into Houses
+	(ID,  StreetAddress, City, State ,MainPhoto, HouseTypeID ,PhotoID, Description)
+    values(5, '1354 main st', 'boise', 'id' , 'images\\com\\downtown\\downtown-storefront.jpg', 'com', 5, 'store font ');
 
 insert into Images
 	(ID, image)
     values(0,'images\\res\\brick\\BH-kitchen.jpg');
+    
+    insert into Images
+	(ID, image)
+    values(0,'images\\res\\brick\\BH-bathroom.jpg');
+    
+    insert into Images
+	(ID, image)
+    values(0,'images\\res\\brick\\BH-bedroom.jpg');
     
     insert into Images
 	(ID, image)
@@ -42,19 +54,43 @@ insert into Images
     
 	insert into Images
 	(ID, image)
+    values(1,'images\\res\\white\\WH-bathroom.jpg');
+    
+    insert into Images
+	(ID, image)
+    values(1,'images\\res\\white\\WH-bedroom.jpg');
+    
+	insert into Images
+	(ID, image)
     values(1,'images\\res\\white\\WH-kitchen.jpg');
     
 	insert into Images
 	(ID, image)
-    values(3,'images\\res\\tiny\\TH-kitchen.jpg');
+    values(2,'images\\res\\tiny\\TH-kitchen.jpg');
     
 	insert into Images
 	(ID, image)
-    values(3,'images\\res\\tiny\\TH-kitchen.jpg');
+    values(2,'images\\res\\tiny\\TH-bathroom.jpg');
+    
+    insert into Images
+	(ID, image)
+    values(2,'images\\res\\tiny\\TH-bedroom.jpg');
+    
+	insert into Images
+	(ID, image)
+    values(2,'images\\res\\tiny\\TH-livingroom.jpg');
     
     insert into Images
 	(ID, image)
     values(4,'images\\res\\mondern\\MH-kitchen.jpg');
+    
+	insert into Images
+	(ID, image)
+    values(4,'images\\res\\mondern\\MH-bathroom.jpg');
+    
+    insert into Images
+	(ID, image)
+    values(4,'images\\res\\mondern\\MH-bedroom.jpg');
     
     insert into Images
 	(ID, image)
--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="jumbotron text-center" style="margin-bottom:0">
+<!-- <div class="jumbotron text-center" style="margin-bottom:0">
   <div class="row justify-content-center">
     <div class="col-sm-6">
       <h4><p class="text-center"> CADAVenturs</p></h4>
@@ -9,4 +9,19 @@
       <p class="text-center">cadaventuresmaintenance@gmail.com</p>
     </div>
   </div>
-</div>
+</div> -->
+
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        <h4><p class="text-center"> CADAVenturs</p></h4>
+        <p class="text-center"> Thank you for checking out our rentals. If you have questions please feel free to contact us.</p>
+      </div>
+      <div class="col-sm-6">
+        <p class="text-center">Email:</p>
+        <p class="text-center">cadaventuresmaintenance@gmail.com</p>
+      </div>
+    </div>
+  </div>
+</footer>
