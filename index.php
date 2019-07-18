34da78039ae8ae64667b3712c409ecc383ff85ef
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-18 16:58:37 -0600

    too many }

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
                           <img src=\"".$pics[$y]."\"  style=\"width:100%; height:50%\">
                         </div>";
               }
-          }
+          
         ?>
 
         <!-- <div class="item active">
