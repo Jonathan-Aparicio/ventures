72e034867c2ba26a9c529bcc1ee466bc3e933f2c
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-18 14:10:06 -0600

    try modle with image

--- generalHouse.php
+++ generalHouse.php
@@ -52,7 +52,7 @@
             break;
           }else{
             echo "<div class=\"col-sm-4\">
-              <img class=\"img-fluid\" src=\"" . $pics[$z]['image'] . "\">
+              <img class=\"img-fluid\" src=\"" . $pics[$z]['image'] . " \" data-toggle=\"modal\" data-target=\"#exampleModal\">
             </div>";
             $z++;
           }
@@ -62,25 +62,26 @@
     }
   ?>
 
-</div>
-</div>
-</div>
-</div>
 
-<!-- <footer class="footer mt-auto py-3">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-sm-6">
-        <h4><p class="text-center"> CADAVenturs</p></h4>
-        <p class="text-center"> Thank you for checking out our rentals. If you have questions please feel free to contact us.</p>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <img src="" class="imagepreview" style="width: 100%;" >
       </div>
-      <div class="col-sm-6">
-        <p class="text-center">Email:</p>
-        <p class="text-center">cadaventuresmaintenance@gmail.com</p>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
-</footer> -->
+</div>
+
+<!-- </div>
+</div>
+</div> -->
+
 
   <?php require_once "footer.php";?>
 
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <div class="container">
     <div class="jumbotron">
   <!-- <h1>Bootstrap Tutorial</h1> -->
-  <p class="text-center">Welcom to CADAVentures rentals curently this website is a school project. All pictures being used are stock photos
+  <p class="text-center">Welcome to CADAVentures rentals curently this website is a school project. All pictures being used are stock photos
     </p>
 </div>
 
