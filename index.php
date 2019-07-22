70cb89a344658def5741f5613be84d2875c5baeb
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-22 11:32:17 -0600

    a

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 </div>
 </div>
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- <div id="myCarousel" class="carousel slide" data-ride="carousel"> -->
       <!-- Indicators -->
       <!-- <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -36,20 +36,20 @@
       </ol> -->
 
       <!-- Wrapper for slides -->
-      <div class="carousel-inner">
+      <!-- <div class="carousel-inner"> -->
         <?php
-              for($y = 0; $y < sizeof($pics); $y++){
-                  echo "<div class=\"carousel-item\">
-                          <img src=\"".$pics[$y]['MainPhoto']."\"  class=\"d-block w-100 h-50\">
-                        </div>";
-              }
+              // for($y = 0; $y < sizeof($pics); $y++){
+              //     echo "<div class=\"carousel-item\">
+              //             <img src=\"".$pics[$y]['MainPhoto']."\"  class=\"d-block w-100 h-50\">
+              //           </div>";
+              // }
 
         ?>
 
-      </div>
+      <!-- </div> -->
 
       <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
         <span class="sr-only">Previous</span>
       </a>
@@ -57,7 +57,33 @@
         <span class="glyphicon glyphicon-chevron-right"></span>
         <span class="sr-only">Next</span>
       </a>
-    </div>
+    </div> -->
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <?php
+          for($y = 0; $y < sizeof($pics); $y++){
+              echo "<div class=\"carousel-item\">
+                      <img src=\"".$pics[$y]['MainPhoto']."\"  class=\"d-block w-100 h-50\">
+                    </div>";
+          }
+
+    ?>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
   </div>
 
 
