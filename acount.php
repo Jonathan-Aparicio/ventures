28b7cfe664bae9d157f2fb1c46f9efbac743525d
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-07-29 13:23:47 -0600

    first acount page

---
+++ acount.php
@@ -0,0 +1,35 @@
+<?php
+  require_once "DAO.php";
+  $dao = new Dao();
+  $user;
+  if (!isset($_SESSION["access_granted"]) && !$_SESSION["access_granted"]) {
+    header("Location:index.php");
+  }else{
+    $user = $dao->getUser(@$_SESSION['email']);
+  }
+ ?>
+<body class="d-flex flex-column h-100">
+  <div>
+    <?php require_once "nav-bar.php";?>
+  </div>
+
+  <div class="container">
+      <?php if($user['Renting'] != NULL): $address = $dao->getInfo((int)$user['Renting']);?>
+      <div class="row justify-content-start">
+        <div class="col-sm-4">
+          <h4>Payment</h4>
+        </div>
+      </div>
+      <div class="row align-items-center justify-content-center">
+
+      </div>
+    <?php else: ?>
+      <div class="row align-items-center justify-content-center" style="height:100%">
+        <p class="text-center">You are curently not renting a property. If this is incorect please email cadaventuresmaintenance@gmail.com with the
+        email you have signed your lease with and we shall update your acount.</p>
+      </div>
+    <?php endif; ?>
+  </div>
+
+  <?php require_once "footer.php";?>
+</body>
