094637c0826197d4f61d46d2a1357074bb8563a6
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-08-15 10:45:41 -0600

    try new fav

--- nav-bar.php
+++ nav-bar.php
@@ -10,7 +10,8 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="index.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-  <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
+  <!-- <link rel="shortcut icon" type="image/ico" href="favicon.ico"/> -->
+  <link rel="icon" href="fav.png" type="image/png" sizes="48x48">
 </head>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
