8a8ca30c03c12f0883d4796559208e9645b261b3
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-08-15 15:01:21 -0600

    fav 7

--- nav-bar.php
+++ nav-bar.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="index.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
   <!-- <link rel="shortcut icon" type="image/ico" href="favicon.ico"/> -->
-  <link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
+  <link rel="icon" href="/favicon.ico"/>
 </head>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
