a8fc8cc1831761ac7be8150a033dfcbf8d208058
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-08-26 15:27:52 -0600

    footer changes

--- footer.php
+++ footer.php
@@ -2,14 +2,18 @@
 <footer class="footer mt-auto py-3">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-sm-6">
+      <div class="col-sm-4">
         <h4><p class="text-center"> CADAVenturs</p></h4>
         <p class="text-center"> Thank you for checking out our rentals. If you have questions please feel free to contact us.</p>
       </div>
-      <div class="col-sm-6">
+      <div class="col-sm-4">
         <p class="text-center">Email:</p>
         <p class="text-center">cadaventuresmaintenance@gmail.com</p>
         <p class="text-center">Jonathan Aparicio<br>jonathandaparicio@gmail.com</p>
+      </div>
+      <div class="col-sm-4">
+        <p class="text-center">@Jonathan Aparicio</p>
+        <p class="text-center">jonathandaparicio@gmail.com</p>
       </div>
     </div>
   </div>
