f3475ab17956c3d2e8f6631853be21535cacdd35
jonathan-aparicio <jonathandaparicio@gmail.com>
2019-08-27 10:19:12 -0600

    forgot to take out my email fomr seond columne

--- footer.php
+++ footer.php
@@ -9,7 +9,6 @@
       <div class="col-sm-4">
         <p class="text-center">Email:</p>
         <p class="text-center">cadaventuresmaintenance@gmail.com</p>
-        <p class="text-center">Jonathan Aparicio<br>jonathandaparicio@gmail.com</p>
       </div>
       <div class="col-sm-4">
         <p class="text-center">@Jonathan Aparicio</p>
